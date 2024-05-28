5d2960e058b0aa80f2336f966ae6a3765823ac02
maitedlapierre <dienefer.2022311842@aluno.iffar.edu.br>
2024-05-28 17:14:08 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,20 @@
 <?php
 
-$pastaDestino = "uploads/";
-$nomeArquivo  = $_FILES['arquivo']['name'];
+//definiu a pasta de destino
+$pastaDestino = "/uploads/";
+var_dump($_FILES);
+
+var_dump($_FILES['arquivo']['size']);
+
+//verificar se tamanho do arquivo é maior que 2mb
+if(($_FILES['arquivo']['size']) > 2000000){
+    echo "O tamanho do arquivo é maior que o limite permitido!";
+    die();
+}
+
+// verificar se o arquivo é uma imagem
 var_dump($_FILES['arquivo']['name']);
-var_dump($_SERVER);
-file_exists($_SERVER . $pastaDestino . $nomeArquivo);
+var_dump(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION)); 
+
 
 ?>
