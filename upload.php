4c67468e4335938143dc26da660ee1a117ecceb8
maitedlapierre <dienefer.2022311842@aluno.iffar.edu.br>
2024-06-04 17:06:59 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,19 +2,44 @@
 
 //definiu a pasta de destino
 $pastaDestino = "/uploads/";
-var_dump($_FILES);
-
-var_dump($_FILES['arquivo']['size']);
 
 //verificar se tamanho do arquivo é maior que 2mb
 if(($_FILES['arquivo']['size']) > 2000000){
     echo "O tamanho do arquivo é maior que o limite permitido!";
+
+//condição de guarda
     die();
 }
 
 // verificar se o arquivo é uma imagem
-var_dump($_FILES['arquivo']['name']);
-var_dump(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION)); 
+$extensao= strtolower (pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
+
+if($extensao != "png" && $extensao != "jpg" && 
+    $extensao != "jpeg" && $extensao != "gif" && 
+    $extensao != "jfif" && $extensao != "svg") {
+    echo "O arquivo não é uma imagem! Apenas selecione arquivos com extensão png,jpg, jpeg, gif, jfif ou svg.";
+
+//condição de guarda
+    die ();
+}
+
+//verificar se é uma imagem de fato
+if(getimagesize($_FILES['arquivo']['tmp_name']) === "false") {
+    echo "Problemas ao enviar a imagem. Tente novamente!";
+    die();
+}
+
+$nomeArquivo= uniqid();
+
+//se deu certo, faz o upload
+$fezUp= move_uploaded_file($_FILES['arquivo']['tmp_name'], __DIR__ . $pastaDestino . $nomeArquivo . "." . $extensao);
+
+if($fezUp == true) {
+    header("location:index.php");
+} else {
+    echo "Erro ao mover arquivo";
+}
+
 
 
 ?>
