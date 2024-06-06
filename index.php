aa0a134542e5b947e9a5988af8ab158734ed737f
maitedlapierre <dienefer.2022311842@aluno.iffar.edu.br>
2024-06-06 15:45:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+ $conexao= mysqli_connect("localhost", "root", "", "upload-arquivos");
+ $slq= "SELECT * FROM arquivo";
+ $resultado= mysqli_query ($conexao, $slq);
+
+ if ($resultado != false) {
+    $arquivos= mysqli_fetch_all ($resultado, MYSQLI_BOTH); 
+ } else {
+    echo "Erro ao executar comando SQL.";
+    die();
+}
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +30,25 @@
         <input type="file" name="arquivo"><br><br>
         <input type="submit" value="Fazer Upload" name="submit">
     </form>
+    <br> <br>
+    <table>
+        <tr>
+            <th>Nome do Arquivo </th>
+            <th colspan= "2"> Opções </th>
+        </tr>
+        </thead>
+        <tbody> 
+            
+            <?php
+            foreach ($arquivos as $arquivo) {
+                echo "<tr> <td>" . $arquivo ['nome_arquivo'] . "</td>"; 
+                echo "<td> <a href='alterar.php?nome_arquivo=" . $arquivo ['nome_arquivo'] . "'>Alterar</td>";
+                echo "<td> <button> Excluir </button> </td></tr>";      
+             }
+
+            ?>
+            </tbody>
+    </table>
 </body>
 
 </html
